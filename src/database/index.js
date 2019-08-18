59e546286d481064af5c5a95068c46508177b2c4
rafael05 <rafaeloliveirasilva05@gmail.com>
2019-08-18 17:13:17 -0300

    addres mongodb

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true })
+mongoose.connect('mongodb://db:27017/chekin', { useNewUrlParser: true })
 mongoose.Promise = global.Promise
 
 module.exports = mongoose 
